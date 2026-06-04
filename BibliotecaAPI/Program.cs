aa85fb9437b03a57d4eb7ffd4fa8f7b690f20261
FedeGove <federico.governatori07@gmail.com>
2026-06-04 12:16:43 +0200

    Add database migration on application startup

--- BibliotecaAPI/Program.cs
+++ BibliotecaAPI/Program.cs
@@ -43,6 +43,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<BibliotecaContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
